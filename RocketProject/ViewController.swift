09b3003f41b3571aeade6da331c1ba165d1ca4ba
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2018-04-27 08:29:11 +0700

    Swift Rocket Project

---
+++ RocketProject/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  RocketProject
+//
+//  Created by Project on 27/04/18.
+//  Copyright © 2018 Project. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var muzmaLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let path = Bundle.main.path(forResource: "muzma-on", ofType: "wav")!
+//        let url = URL(fileURLWithPath: path)
+//        do {
+//            player = try AVAudioPlayer(contentsOf: url)
+//            player.prepareToPlay()
+//
+//        }catch let error as NSError{
+//            print(error.description)
+//        }
+        
+    }
+    
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        
+     //   player.play()
+        
+        UIView.animate(withDuration: 2.3, animations:{
+            self.rocket.frame = CGRect(x: 0, y:100, width: 143, height:402)
+        }) {(finished) in
+            self.muzmaLbl.isHidden = false
+            self.onLbl.isHighlighted =  false
+        }
+    }
+
+}
+
